facddb9e335e379400484281e5e85801a662e466
Robrizo <113062243+Robrizo@users.noreply.github.com>
2022-11-05 09:42:42 +0200

    Create interest.c

---
+++ interest.c
@@ -0,0 +1,46 @@
+/*Program Calculating compound interest
+Using user defined function
+Developed by Robert Chunga @2022*/
+
+#include <stdio.h>
+#include <math.h>
+
+//function prototype
+float interest(float a, float b, float c, float d);
+
+int main()
+{
+    //variable declaration
+    float p, r, n, t;
+    int redo;
+
+    do{
+    system("cls");
+    //getting input from user
+    printf("COMPOUND INTEREST CALCULATOR\n\n");
+    printf("Enter principal amount:\n>>");
+    scanf("%f", &p);
+    printf("Enter rate: \n>>");
+    scanf("%f", &r);
+    printf("Enter times a year: \n>>");
+    scanf("%f", &t);
+    printf("Enter number of years: \n>>");
+    scanf("%f", &n);
+
+    //printing results
+    system("cls");
+    printf("\nYour interest is K%.2f\n", interest(p,r,t,n)); //function calling
+    printf("\nDo you want to perform another Calculation?\n\nPress 1 to continue or 2 to exit\n->>");
+    scanf("%d", &redo);
+
+    } while(redo != 2);
+    system("cls");
+    return 0;
+}
+//function definition
+float interest(float a, float b, float c, float d)
+{
+    float coint = a * pow((1 + (b/c)),(c * d));
+
+    return coint;
+}
